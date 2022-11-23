6002c2668033415add93d7ecf13899e4c218a956
Yogesh-Dewangan <yogdev0dew@gmail.com>
2022-11-23 10:27:42 +0530

    added contact and user models

---
+++ server/models/Contacts.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const contactSchema = new Schema({
+    name: {type: String, required: true},
+    designation: {type: String, required: true},
+    company: {type: String, required: true},
+    industry: {type: String, required: true},
+    email: {type: String, required: true},
+    phoneNumber: {type: Number, required: true},
+    country: {type: String, required: true}
+});
+
+const ContactModel = mongoose.model('Contact', contactSchema);
+
+module.exports = ContactModel
