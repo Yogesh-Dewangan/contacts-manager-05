c0cbb95715f270ec552d2d281f1b9cbc33566029
Ankit Saxena <juetankit@gmail.com>
2022-11-23 16:15:50 +0530

    Adding new user with hashed password accomplished

--- server/routes/register.js
+++ server/routes/register.js
@@ -22,17 +22,42 @@ router.post("/",
                         message:errors.array()
                     });
                 }else {
-                    if(req.body.password != req.body.confirm_password){
+                    if(req.body.password != req.body.confirm_password)
                         return res.status(400).json({
                             message:"Passwords do not match."
                         })
-                    }
-                }
-            }
 
-        }catch(e){
+                        bcrypt.hash(req.body.password,10,async(err,hash)=>{
+                            if(err){
+                                console.log(err.message)
+                            }
+                            await data.create({
+                                email:req.body.email,
+                                password:hash
+                            });
+                        });
+                        res.status(200).json({
+                            status:"Registration Successful",
+                            message:"User Can Login Now"
+                        });
+                }
+              }  else{
+                        res.status(400).json({
+                            status:"Registration unsuccessful",
+                            message:"User already exists"
+                        })
+                    }
+
+            }catch(e){
+                res.status(500).json({
+                    status:"failed",
+                    message:e.message
+
+                })
             
         }
 
     }
-    )
+    );
+
+    module.exports = router;
