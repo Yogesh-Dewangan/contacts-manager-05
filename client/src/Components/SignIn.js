ba02c7128fde7cd001ce5c503a597c7707e64c77
Ankit Saxena <juetankit@gmail.com>
2022-11-24 21:37:15 +0530

    Sign In Component added

--- client/src/Components/SignIn.js
+++ client/src/Components/SignIn.js
@@ -1,1 +1,54 @@
-console.log("Ankit")
+import React ,{useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import { useAPI } from './Context'
+import './SignIn.css'
+
+const SignIn=()=>{
+    const [login ,setLogin]=useState({
+        email:"",
+        password:""
+    });
+
+    const {loginUser}=useAPI();
+
+    const handleChange = (e) => {
+        setLogin((curr) => ({ ...curr, [e.target.name]: e.target.value }));
+      };
+      const UserLogin = () => {
+        loginUser(login);
+      };
+
+    return(
+        <div className='login-page'>
+            <h1>Logo</h1>
+            <p>Enter your credentials to access your account</p>
+            <div className='login-container'>
+                <form onSubmit={(e)=>e.preventDefault()}>
+                <input
+                    onChange={(e) => handleChange(e)}
+                    type="email"
+                    name="email"
+                    className="email"
+                />
+
+                <input
+                    onChange={(e) => handleChange(e)}
+                    type="password"
+                    name="password"
+                    className="password"
+                    placeholder="Password"
+                />
+
+                <button onClick={UserLogin} className="signIn">
+                    Sign In
+                </button>
+
+            </form>
+            <Link id="signup" to="/signup">Sign Up</Link>            
+            </div>
+
+        </div>
+    )
+}
+
+export default SignIn;
