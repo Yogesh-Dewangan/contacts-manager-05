6f44e33be48fa93c8078de64d8848a0019f34467
Yogesh-Dewangan <yogdev0dew@gmail.com>
2022-11-25 17:20:29 +0530

    comment out unused lines

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -3,22 +3,22 @@ const express = require("express")
 let contacts = require("../models/Contacts")
 const router = express.Router()
 
-router.post("/import", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
-        const contactlist = await contacts.create({
-            name: req.body.name ,
-            designation: req.body.designation ,
-            company: req.body.company ,
-            industry: req.body.industry  ,
-            email:req.body.email  ,
-            phoneNumber: req.body.phoneNumber  ,
-            country: req.body.country
-        })
+        // const contactlist = await contacts.create({
+        //     name: req.body.name ,
+        //     designation: req.body.designation ,
+        //     company: req.body.company ,
+        //     industry: req.body.industry  ,
+        //     email:req.body.email  ,
+        //     phoneNumber: req.body.phoneNumber  ,
+        //     country: req.body.country
+        // })
         console.log(req.body)
         res.status(200).json({
 
-            status: "success",
-            data: contactlist
+            status: "success"
+            // data: contactlist
         })
 
     } catch (e) {
