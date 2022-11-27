eb807339d0a62f93b7a5441483b3bdc098b4f7ba
vineetha <yenugulavineetha2001@gmail.com>
2022-11-27 17:58:29 +0530

    Added handlecheckbox to contactpage

--- client/src/Components/contact-page-components/contactpage.jsx
+++ client/src/Components/contact-page-components/contactpage.jsx
@@ -14,53 +14,17 @@ const Contacts = () => {
     const [deleteTrigger, setDeleteTrigger] = useState(false);
     const [selection, setselectionarr] = useState([])
     // const [arrayOfContactsId, setArrayOfContactsId] = useState([]);
-
-    var data = [
-
-        {
-            _id: 6542786873,
-            name: "vineetha",
-            designation: "student",
-            company: "10x",
-            industry: "IT",
-            email: "vineetha@gmail.com",
-            phoneNumber: 572348778,
-            country: "India"
-        },
-        {
-            _id: 654278684373,
-            name: "ankit",
-            designation: "student",
-            company: "10x",
-            industry: "IT",
-            email: "ankit@gmail.com",
-            phoneNumber: 767486765,
-            country: "India"
-        },
-        {
-            _id: 65427868784373,
-            name: "yogesh",
-            designation: "student",
-            company: "10x",
-            industry: "IT",
-            email: "yogesh@gmail.com",
-            phoneNumber: 76758628,
-            country: "India"
-        }
-    ]
-
     const [ischecked, setischecked] = useState([])
-    const [seletallpage,setselectedallpage]=useState([])
-    const [contacts, setcontacts] = useState([])
-    //const [perpage,setperpage]=useState([])
-    const [currentpage, setcurrentpage] = useState(1)//1,2,3
-    const [itemperpage, setitemperpage] = useState(5)//how many items per page
-    const [currentItems, setCurrentItems] = useState([]);
 
+    //-----------------------------------pagination-----------
+    const [contacts, setcontacts] = useState([])
+    const [currentpage, setcurrentpage] = useState(1)//1,2,3
+    const [itemperpage, setitemperpage] = useState(5)//how many items per page  //const [currentItems, setCurrentItems] = useState([]);
 
     const [pagenumberlimit, setpagenumberlimit] = useState(5)
     const [maxpagenumberlimit, setmaxpagenumberlimit] = useState(1)//
     const [minpagenumberlimit, setminpagenumberlimit] = useState(0)
+
 
     useEffect(() => {
         getContacts()
@@ -75,102 +39,15 @@ const Contacts = () => {
 
     }, [])
 
-const handlecheckbox=(e)=>{
-    const {value,checked}=e.target
-    console.log(value);
-    if(checked){
-        setischecked([...ischecked,value])
-    }
-    else{//uncheck
-        setischecked(ischecked.filter((e)=>e!==value))//finding the value of 
-    }
-}
-
-const allDElete=async()=>{
-    console.log(ischecked)
-}
-// console.log(ischecked)
-
-
-
-
-    const handleclick = (event) => {
-        setcurrentpage(Number(event.target.id))
-    }
-
     const pages = []
     for (let i = 1; i < Math.ceil(contacts.length / itemperpage); i++) {
         pages.push(i)
     }
 
-    const renderconts = (conts) => {
-        return (
-
-            <>{
-                contacts.map((contact, index) => {
-                    let len = contacts.length
-                    let val = index
-                    return (
-                        <tr key={index}>
-                            <td className="tdinp"> 
-                            <input type="checkbox" value={contact._id} checked={contact.ischecked}
-                                onChange={(e) => {
-                                    handlecheckbox(e)
-                                }} /></td>
-                            <td className="td">{contact.name}</td>
-                            <td className="td">{contact.designation}</td>
-                            <td className="td">{contact.company}</td>
-                            <td className="td">{contact.industry}</td>
-                            <td className="tdemail">
-                                <div className="tooltip">
-                                    {contact.email}
-                                    <span className="tooltiptext"> {contact.email}</span>
-                                </div>
-
-                            </td>
-                            <td className="td">{contact.phoneNumber}</td>
-                            <td className="td">{contact.country}</td>
-                            <td className="td" style={{ display: "flex", flexDirection: "row" }}>
-
-                                <button onClick={() => setDeleteTrigger(true)}><img className="actionimage" src={deleted} alt="deleteImg" /></button>
-                                <button><img src={edited} alt="editImg" className="actionimage" onClick={allDElete}/></button>
-                            </td>
-                        </tr>
-                    )
-                })
-            }
-
-            </>
-        )
-
-    }
-
     const indexoflatitem = currentpage * itemperpage;
     const indexoffirstitem = indexoflatitem - itemperpage;
-    // const currentItem = contacts.slice(indexoffirstitem, indexoflatitem)
+    const currentItem = contacts.slice(indexoffirstitem, indexoflatitem)
 
-    useEffect(() => {
-        setCurrentItems(contacts.slice(indexoffirstitem, indexoflatitem))
-    }, [contacts])
-
-    const renderpagenumbers = pages.map(pgnumber => {
-        if (pgnumber < maxpagenumberlimit + 1 && pgnumber > minpagenumberlimit) {
-            return (
-                <li key={pgnumber} id={pgnumber} onClick={handleclick}
-                    className={currentpage == pgnumber ? "active" : null} >
-                    {pgnumber}
-                </li>
-            )
-        }
-        else {
-            return null
-        }
-
-    })
-    
-    // const pageHandler=(pagenumber)=>{
-    //     setperpage(currentItems)
-    // }
     const handlenext = () => {
         setcurrentpage(currentpage + 1)
         if (currentpage + 1 > maxpagenumberlimit) {
@@ -194,29 +71,103 @@ const allDElete=async()=>{
         pageDecrementbtn = <li onclick={handleprev}>&hellip;</li>
     }
 
+    const handlecheckbox = (e) => {
+        const { value, checked } = e.target
+        if (value == 'all') {
+            currentItem.map((contact) => {
+                contact.checked = true
+            })
+        }
+        else {
+            console.log(value);
+            if (checked) {
+                setischecked([...ischecked, value])
+            }
+            else {//uncheck
+                setischecked(ischecked.filter((e) => e !== value))//finding the value of
+            }
+        }
+
+
+    }
+
+    const allDElete = async () => {
+        console.log(ischecked)
+    }
+
+    const handleclick = (event) => {
+        setcurrentpage(Number(event.target.id))
+    }
+
+
+    const renderconts = (conts) => {
+        return (<>
+        { contacts.map((contact, index) => {
+            let len = contacts.length
+            let val = index
+            return (
+                //individual  checkbox 
+                <tr key={index}>
+                    <td className="tdinp">
+                        <input type="checkbox" value={contact._id} checked={contact.ischecked}
+                            onChange={(e) => {
+                                handlecheckbox(e)
+                            }}/></td>
+                    <td className="td">{contact.name}</td>
+                    <td className="td">{contact.designation}</td>
+                    <td className="td">{contact.company}</td>
+                    <td className="td">{contact.industry}</td>
+                    {/* //tooltip */}
+                    <td className="tdemail">
+                        <div className="tooltip">
+                            {contact.email}
+                            <span className="tooltiptext"> {contact.email}</span>
+                        </div>
+                    </td>
+                    <td className="td">{contact.phoneNumber}</td>
+                    <td className="td">{contact.country}</td>
+                    {/* action buttons */}
+                    <td className="td" style={{ display: "flex", flexDirection: "row" }}>
+                        <button onClick={() => setDeleteTrigger(true)}><img className="actionimage" src={deleted} alt="deleteImg" /></button>
+                        <button><img src={edited} alt="editImg" className="actionimage" onClick={allDElete} /></button>
+                    </td>
+                </tr>
+            )
+        })} </>)
+    }
+
+
+    const renderpagenumbers = pages.map(pgnumber => {
+        if (pgnumber < maxpagenumberlimit + 1 && pgnumber > minpagenumberlimit) {
+            return (
+                <li key={pgnumber} id={pgnumber} onClick={handleclick} className={currentpage == pgnumber ? "active" : null} >
+                    {pgnumber}
+                </li>
+            )
+        } else {
+            return null
+        }
+    })
+
+
     return (
         <div className="main">
-            <div>
+            <div className="nav">
                 <NavBar getproducts={getContacts} setcontacts={setcontacts} />
             </div>
             <div className="contacts">
                 <div className="btn-container">
                     <Buttons deleteTrigger={deleteTrigger} setDeleteTrigger={setDeleteTrigger} />
                 </div>
-
-
                 <table>
                     <thead>
-
-                        <Cardheaders setDeleteTrigger={setDeleteTrigger} 
-                        setselectedallpage={setselectedallpage}
-                        handlecheckbox={handlecheckbox}
-                        ischecked={ischecked}
-                        currentItems={currentItems}/>
+                        <Cardheaders setDeleteTrigger={setDeleteTrigger}
+                            handlecheckbox={handlecheckbox}
+                            currentItem={currentItem} />
                     </thead>
                     <tbody>
                         {
-                            renderconts(currentItems)
+                            renderconts(currentItem)
                         }
                     </tbody>
 
