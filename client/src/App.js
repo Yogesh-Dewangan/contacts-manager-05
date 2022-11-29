6f2754a141dd2635e9630e9a8e2eb7272d1f7a0a
Yogesh-Dewangan <yogdev0dew@gmail.com>
2022-11-29 17:00:43 +0530

    added context rapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { APIContextProvider } from './Components/Context';
 import MainContactsPage from './Components/mainContactsPage';
-import UseSession from './Components/SessionUtils';
 import SignIn from './Components/SignIn';
 import SignUp from './Components/SignUp';
+import { UserSessionContextProvider } from './Components/Context';
 
 
 function App() {
 
   return <div className="App">
-    {/* <APIContextProvider> */}
     <BrowserRouter>
+      <UserSessionContextProvider>
       <Routes>
         <Route path='/' element={<SignIn />} />
         <Route path='/signup' element={<SignUp/>} />
-        <Route path='/contact' element={
-          <UseSession>
-            <MainContactsPage/>
-          </UseSession>
-        }
+        <Route path='/contact' element={<MainContactsPage/>}
         />
       </Routes>
+      </UserSessionContextProvider>
     </BrowserRouter>
-    {/* </APIContextProvider> */}
   </div>
 }
 
