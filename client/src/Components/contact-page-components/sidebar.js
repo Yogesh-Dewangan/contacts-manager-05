aa1575719dc660cefa7e90f5656f9fe183fcb437
Yogesh-Dewangan <yogdev0dew@gmail.com>
2022-11-29 16:51:00 +0530

    modified logout function

--- client/src/Components/contact-page-components/sidebar.js
+++ client/src/Components/contact-page-components/sidebar.js
@@ -2,10 +2,13 @@ import React  from "react"
 import logo from "./images/logoforcm.png"
 import logout from "./images/im/logout.png"
 import contact from "./images/contact.png"
-import { Link , useNavigate} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
+import { useSessionContext } from "../Context"
 
 const Sidebar=()=>{
-    const navigate=useNavigate();
+    const navigate = useNavigate();
+    const {setUserData} = useSessionContext()
+
     return(
         <div className="side-bar">
             <div>
@@ -13,26 +16,22 @@ const Sidebar=()=>{
                     <img src={logo} styles={{height:"30px",width:"50px"}} alt="logoImg"/>
                 </div>
                 <div className="dash">
-                <h3 className="dashboard">
-                    
-                     Dashboard</h3>
+                <h3 className="dashboard">Dashboard</h3>
                 <div className="total-contactstext">
                 <img src={contact} className="contactImg" alt="contactImg"/>
                     <div>Total contacts </div></div>
                 </div>
                 <footer className="footer">
-                    <Link to="/">
                     <button className="logoutbtn" onClick={() => {
-                            // navigate("/");
+                            setUserData(null);
+                            navigate("/");
                             localStorage.clear("token");
+                            localStorage.clear("userEmail");
                             // window.location.reload();
                           }} style={{display:"flex",flexDirection:"row",width:"80%",marginLeft:"10px"}}>
                         <img src={logout} alt="logoutImg"/>
                         <div className="logtext">Logout</div>
                     </button>
-                    </Link>
-                    
-                    
                 </footer>
             </div>
         </div>
