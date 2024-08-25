4b72a2c75ff301e952768851b0bb60b4ebf23d44
Yogesh Dewangan <yogdev0dew@gmail.com>
2024-08-25 14:23:36 +0530

    some improvement

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,67 +1,68 @@
-const express=require("express")
-const bcrypt=require("bcrypt")
-const router=express.Router();
-const user=require("../models/Users")
-const jwt=require("jsonwebtoken")
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const user = require("../models/Users");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const process = require('process');
+const process = require("process");
 const secret = process.env.SECRET;
+
+let SESSIONID = "";
+
 router.use(express.json());
-router.use(express.urlencoded({extended:true}));
-let SESSIONID = '';
+router.use(express.urlencoded({ extended: true }));
 
-router.post("/", async(req,res)=>{
-    try{
-    const email=req.body.email;
-    const password=req.body.password;
-    const userData=await user.findOne({email})
+router.post("/", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const userData = await user.findOne({ email });
 
-    if(userData){
-        bcrypt.compare(password, userData.password, async (err, result) => {
-            if(err) {
-                return res.status(400).json({
-                    status: "Failed",
-                    message: err.message
-                })
-            }
-            if(result){
-                const token=jwt.sign(
-                    {
-                        exp: Math.floor(Date.now()/1000) + 60*60,
-                        data: userData._id
-                    },
-                    secret
-                );
-                SESSIONID = token;
-                return res.status(200).json({
-                    status:"Login Successful",
-                    token
-                });
-            }
-        
-    })
-    }else{
-        return res.status(400).json({
+    if (userData) {
+      bcrypt.compare(password, userData.password, async (err, result) => {
+        if (err) {
+          return res.status(400).json({
             status: "Failed",
-            message: "Invalid credentials! Please provide valid email/password"
-        })
-    }
-} catch(e) {
-    res.status(500).json({
-        status: "Failed",
-        message: e.message
-    })
-}
-    
-})
-
-router.get('/get-current-user', (req, res) => {
-    const sessionId = req.query.sessionId;
-    if(sessionId === SESSIONID) {
-        res.status(200).send(true);
+            message: err.message,
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              exp: Math.floor(Date.now() / 1000) + 60 * 60,
+              data: userData._id,
+            },
+            secret
+          );
+          SESSIONID = token;
+          return res.status(200).json({
+            status: "Success",
+            message: "Login Successful",
+            token,
+          });
+        }
+      });
     } else {
-        res.status(401).send(false);
-    } 
-})
+      return res.status(400).json({
+        status: "Failed",
+        message: "Invalid credentials! Please provide valid email/password",
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      status: "Failed",
+      message: e.message,
+    });
+  }
+});
+
+router.get("/get-current-user", (req, res) => {
+  const sessionId = req.query.sessionId;
+  if (sessionId === SESSIONID) {
+    res.status(200).send(true);
+  } else {
+    res.status(401).send(false);
+  }
+});
 
 module.exports = router;
